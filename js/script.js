bb0e8fcd254e792ac51ddbe23fdedb30b3fe1a88
MasterJam1013 <jamalrashad1013@gmail.com>
2024-02-15 12:31:48 -0500

    css update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+// open sidebar funct
+function openSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+// close sidebar funct
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
+
 var sidebar = document.getElementById("sidebar");
 var burgerBtn = document.getElementById("desktop-burgerbtn");
 
